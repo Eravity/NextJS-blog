e8eefc2978d13339f5179d1925492f59e3305cab
Eravity <cebotaricristian92@gmail.com>
2024-12-27 17:04:51 +0200

    Add error handling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,7 @@
 
 export default function Error() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="absolute inset-20 flex items-center justify-center">
       <h1 className="text-4xl font-semibold">Something went wrong 😣</h1>
     </div>
   );
